eda56efd0c129b8104cc7a5f0856b9058f532618
ashutosh9897794237 <113291959+ashutosh9897794237@users.noreply.github.com>
2023-07-19 17:00:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 //your JS code here. If required.
-//your JS code here. If required.
-const para = document.getElementById("timer");
- const d = new Date();
-//  this is for date 
- var date = d.getDate();
- var month = d.getMonth();
- var year = d.getFullYear();
-//  this is for time;
-var hour = d.getMonth();
-var min = d.getMinutes();
-var sec = d.getSeconds();
-
-date = date < 10 ? "0"+date : date ;
-month = month < 10 ? "0"+ month : month ;
-min = min < 10 ? "0"+ min : min ;
-
-para.innerHTML = date+"/"+month+"/"+year+", "+hour+":"+min+":"+sec ;
-console.log(para.innerHtml);
+ var myDate = new Date();
+        var myDay = myDate.getDay();
+        
+        // Array of days.
+        var weekday = ['Sunday', 'Monday', 'Tuesday',
+            'Wednesday', 'Thursday', 'Friday', 'Saturday'
+        ];
+        document.write("Today is : " + weekday[myDay]);
+        document.write("<br/>");
+        
+        // get hour value.
+        var hours = myDate.getHours();
+        var ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        var minutes = myDate.getMinutes();
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        var myTime = hours + " " + ampm + " : " + minutes + 
+            " : " + myDate.getMilliseconds();
+        document.write("\tCurrent time is : " + myTime);
