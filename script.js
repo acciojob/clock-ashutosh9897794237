9cdc4a60b5441ed20200ea0272c2b598828088fa
ashutosh9897794237 <113291959+ashutosh9897794237@users.noreply.github.com>
2023-07-19 16:50:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const para = document.getElementById("timer");
+ const d = new Date();
+//  this is for date 
+ var date = d.getDate();
+ var month = d.getMonth();
+ var year = d.getFullYear();
+//  this is for time;
+var hour = d.getMonth();
+var min = d.getMinutes();
+var sec = d.getSeconds();
+
+date = date < 10 ? "0"+date : date ;
+month = month < 10 ? "0"+ month : month ;
+min = min < 10 ? "0"+ min : min ;
+
+para.innerHTML = date+"/"+month+"/"+year+", "+hour+":"+min+":"+sec ;
+console.log(para.innerHtml);
